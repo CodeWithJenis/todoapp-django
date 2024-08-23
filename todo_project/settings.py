6be65e0f0042f3d6992a04a16959c6017169e301
Jenish Ghimire <jenishghimire783@gmail.com>
2024-08-23 09:35:12 +0545

    Now error free

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Security settings
 SECRET_KEY = env('SECRET_KEY', default='your-secret-key')
 DEBUG = env.bool('DJANGO_DEBUG', default=True)
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 # Database configuration
 DATABASES = {
