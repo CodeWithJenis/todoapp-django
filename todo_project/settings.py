ea66145950e4b9c5bb730b4b52c16cf5b5b7a77d
Jenish Ghimire <jenishghimire783@gmail.com>
2024-08-23 09:17:21 +0545

    Change in settings.py

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -1,108 +1,98 @@
-from django.core.management.utils import get_random_secret_key
 from pathlib import Path
-from dotenv import load_dotenv
+import environ
 import dj_database_url
-import os
+
+# Initialize environment variables
+env = environ.Env()
+environ.Env.read_env()  # Reads .env file
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-load_dotenv(".env")
-SECRET_KEY = "-5l!2lqk%89ut6(qu+o%ky2d9r7i!zy37(q(eiyfs7$$)@=fd7"
-DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
-SECURE_HSTS_SECONDS = 31536000
-SECURE_SSL_REDIRECT = not DEBUG
-SESSION_COOKIE_SECURE = not DEBUG
-CSRF_COOKIE_SECURE = not DEBUG
-SECURE_HSTS_INCLUDE_SUBDOMAINS = not DEBUG
-SECURE_HSTS_PRELOAD = not DEBUG
+# Security settings
+SECRET_KEY = env('SECRET_KEY', default='your-secret-key')
+DEBUG = env.bool('DJANGO_DEBUG', default=True)
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'todoapp-django-hvzk.onrender.com']
 
-ALLOWED_HOSTS = ["todoapp-django-hvzk.onrender.com"]
+# Database configuration
+DATABASES = {
+    'default': env.db()  # Reads DATABASE_URL from .env file
+}
 
-
+# Application definition
 INSTALLED_APPS = [
-    "todo_app",
-    "django.contrib.admin",
-    "django.contrib.auth",
-    "django.contrib.contenttypes",
-    "django.contrib.sessions",
-    "django.contrib.messages",
-    "django.contrib.staticfiles",
+    'todo_app',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
 ]
 
 MIDDLEWARE = [
-    "django.middleware.security.SecurityMiddleware",
-    "django.contrib.sessions.middleware.SessionMiddleware",
-    "django.middleware.common.CommonMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware",
-    "django.contrib.auth.middleware.AuthenticationMiddleware",
-    "django.contrib.messages.middleware.MessageMiddleware",
-    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = "todo_project.urls"
+ROOT_URLCONF = 'todo_project.urls'
 
 TEMPLATES = [
     {
-        "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / "templates"],
-        "APP_DIRS": True,
-        "OPTIONS": {
-            "context_processors": [
-                "django.template.context_processors.debug",
-                "django.template.context_processors.request",
-                "django.contrib.auth.context_processors.auth",
-                "django.contrib.messages.context_processors.messages",
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / 'templates'],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = "todo_project.wsgi.application"
+WSGI_APPLICATION = 'todo_project.wsgi.application'
 
-
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": os.environ.get("PGNAME"),
-#         "USER": os.environ.get("PGUSER"),
-#         "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
-#         "HOST": "localhost" if DEBUG else os.environ.get("PGHOST"),
-#         "PORT": os.environ.get("5432"),
-#     }
-# }
-DATABASES = {"default": dj_database_url.parse(os.environ.get("DATABASE_URL"))}
-
+# Password validation
 AUTH_PASSWORD_VALIDATORS = [
     {
-        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
     {
-        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
     },
     {
-        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
     },
     {
-        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
 
-
-LANGUAGE_CODE = "en-us"
-
-TIME_ZONE = "UTC"
-
+# Internationalization
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'UTC'
 USE_I18N = True
-
 USE_TZ = True
 
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
-STATIC_URL = "/static/"
-STATIC_ROOT = BASE_DIR / "staticfiles"
-STATICFILES_DIRS = [BASE_DIR / "static"]
-MEDIA_URL = "media/"
+# Media files
+MEDIA_URL = '/media/'
 
-DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-LOGIN_REDIRECT_URL = "todo_app:home"
-LOGOUT_REDIRECT_URL = "authentication:login"
-LOGIN_URL = "authentication:login"
+# Default primary key field type
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Login URLs
+LOGIN_REDIRECT_URL = 'todo_app:home'
+LOGOUT_REDIRECT_URL = 'authentication:login'
+LOGIN_URL = 'authentication:login'
